43a690dd126eeeae79ad7c31ef0eafa4158b47ca
Dobtz <limitlezz7@gmail.com>
2021-01-09 14:39:50 +0200

    Implementing search by name

---
+++ web/sites/default/files/php/twig/5ff993b940edf_time.html.twig_We0dLc4bXy8nr7vSheJgcicQI/Hw5wMD5C-IlLDm3GiNJu-PknR7YY1quj2dmzY3nbiI4.php
@@ -0,0 +1,89 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* core/themes/stable/templates/field/time.html.twig */
+class __TwigTemplate_374f3c11dbba942f98504212ec9c3f4777bbe9124548ab0abb5e06b417f5720f extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $tags = array();
+        $filters = array("escape" => 22);
+        $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                [],
+                ['escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 22
+        echo "<time";
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["attributes"] ?? null), 22, $this->source), "html", null, true);
+        echo ">";
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["text"] ?? null), 22, $this->source), "html", null, true);
+        echo "</time>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "core/themes/stable/templates/field/time.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  62 => 22,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "core/themes/stable/templates/field/time.html.twig", "D:\\wamp64\\www\\DrupalSite\\web\\core\\themes\\stable\\templates\\field\\time.html.twig");
+    }
+}
