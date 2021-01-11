06ab4a3ddee41f699365bea1a258eb7d4bd56fdc
Dobtz <limitlezz7@gmail.com>
2021-01-11 12:10:23 +0200

    Update custom module & add categories

---
+++ web/sites/default/files/php/twig/5ff9a4bc7ae4e_views-view-field.html.twi_mePB3oeHeJVGul23R4ppku76R/0OXVuSWiaWR-74PX46bei1KbhaXmgjrJ1ja2KcYjwAk.php
@@ -0,0 +1,84 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* core/themes/stable/templates/views/views-view-field.html.twig */
+class __TwigTemplate_fe9ddd6b20037c2db28bbc57d1a7aecc092d8ebd349035333cbc7b327b586001 extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $tags = array();
+        $filters = array("escape" => 21);
+        $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                [],
+                ['escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 21
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["output"] ?? null), 21, $this->source), "html", null, true);
+    }
+
+    public function getTemplateName()
+    {
+        return "core/themes/stable/templates/views/views-view-field.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  62 => 21,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "core/themes/stable/templates/views/views-view-field.html.twig", "D:\\wamp64\\www\\DrupalSite\\web\\core\\themes\\stable\\templates\\views\\views-view-field.html.twig");
+    }
+}
